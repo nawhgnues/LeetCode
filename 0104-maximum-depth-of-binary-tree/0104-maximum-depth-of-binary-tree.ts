0211ec55cc4f282aa43673548a8e2cc945c7533a
seunghwan <123640210+nawhgnues@users.noreply.github.com>
2023-09-22 22:36:47 +0900

    Time: 51 ms (97.46%), Space: 46.5 MB (46.30%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,14 @@
+function maxDepth(root: TreeNode | null): number {
+    if(!root) return 0;
+    
+    return DFS(root, 0)
+}; 
+
+function DFS(root: TreeNode | null, count : number): number {
+    if(!root){
+        return count;
+    }
+    
+    count++;
+    return Math.max(DFS(root.left, count), DFS(root.right, count))
+}
